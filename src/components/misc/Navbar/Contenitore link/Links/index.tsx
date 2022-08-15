8a2404f2354dfe5423ebe47b9b17d244d2ca5168
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-08-15 23:37:09 +0200

    Added icons into the navbar

--- src/components/misc/Navbar/Contenitore link/Links/index.tsx
+++ src/components/misc/Navbar/Contenitore link/Links/index.tsx
@@ -1,6 +1,9 @@
 import { NextComponentType } from 'next'
 import Link from 'next/link';
 import style from "./index.module.scss";
+import { MdDesignServices } from "react-icons/md/"
+import { TbBrandGithub, TbTags, TbMoodHappy } from "react-icons/tb";
+
 
 type Props = {}
 
@@ -8,10 +11,33 @@ type Props = {}
 const Links: NextComponentType<Props> = () => {
     return (
         <div className={style.links}>
-            <Link href="#"><a className={style.link}>About me</a></Link>
-            <Link href="#"><a className={style.link}>Progects</a></Link>
-            <Link href="#"><a className={style.link}>Services</a></Link>
-            <Link href="#"><a className={style.link}>GitHub</a></Link>
+            <Link href="#">
+                <a className={style.link}>
+                    <TbMoodHappy className={style.icon} />
+                    About me
+                </a>
+            </Link>
+
+            <Link href="#">
+                <a className={style.link}>
+                    <MdDesignServices className={style.icon} />
+                    Projects
+                </a>
+            </Link>
+
+            <Link href="#">
+                <a className={style.link}>
+                    <TbTags className={style.icon} />
+                    Services
+                </a>
+            </Link>
+
+            <Link href="#">
+                <a className={style.link}>
+                    <TbBrandGithub className={style.icon} />
+                    GitHub
+                </a>
+            </Link>
         </div>
     )
 }
