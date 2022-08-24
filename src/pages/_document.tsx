e959d21ee445ec8628e5e2ef1b7fa615bfaf1696
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-08-24 16:28:58 +0200

    Added a font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,6 +16,8 @@ class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;1,500&display=swap" rel="stylesheet" />
+
+                    <link href="https://fonts.googleapis.com/css2?family=DynaPuff&display=swap" rel="stylesheet" />
                 </Head>
                 <body>
                     <Main />
