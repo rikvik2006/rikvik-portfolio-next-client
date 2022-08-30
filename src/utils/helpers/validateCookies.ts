cb722ee1236a1458dac7e11e59de34e61258a873
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-08-30 14:02:21 +0200

    Recrated the validete cookies now with context

--- src/utils/helpers/validateCookies.ts
+++ src/utils/helpers/validateCookies.ts
@@ -1,2 +1,10 @@
-export const validateCookies = async () => {
+import { GetServerSidePropsContext } from "next";
+
+export const validateCookies = async (ctx: GetServerSidePropsContext) => {
+    const sessionID = ctx.req.cookies["connect.sid"]
+    console.log(sessionID);
+
+    return sessionID ? {
+        Cookie: `connect.sid=${sessionID}`
+    } : false;
 }
