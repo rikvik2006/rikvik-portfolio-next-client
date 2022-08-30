95584bab3916e820c128fa18536e32f6b5900045
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-08-31 01:05:04 +0200

    Started the creation fo personal page

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["imgur.com/", "i.imgur.com"],
+    domains: ["imgur.com", "i.imgur.com"],
   },
 }
 
--- src/pages/personalpage/index.tsx
+++ src/pages/personalpage/index.tsx
@@ -2,6 +2,8 @@ import { GetServerSidePropsContext, NextPage } from "next";
 import { getAvatar } from "../../utils/helpers/getAvatar";
 import { validateCookies } from "../../utils/helpers/validateCookies";
 import { Avatar } from "../../utils/helpers/getAvatar";
+import style from "./index.module.scss";
+import Image from "next/image";
 
 type Prop = {
     avatar: Avatar;
@@ -10,10 +12,24 @@ type Prop = {
 const PersonalPage: NextPage<Prop> = ({ avatar }) => {
 
     return (
-        <>
-            <div>Persona page</div>
-            <div>{avatar.image}</div>
-        </>
+        <div className={style.container}>
+            <div className={style.avatarContainer}>
+                <div className={style.subAvatarContainer}>
+
+                    <Image className={style.avatar}
+                        src={avatar.image}
+                        alt={avatar.image}
+                        height={290}
+                        width={290}
+                    />
+                    <div className={style.informationContainer}>
+                        <h3>Riccardo Bussano</h3>
+                        <p>rikvik2006</p>
+                    </div>
+                </div>
+            </div>
+        </div >
+
     )
 }
 
