69555f70e0616f871e57959ba41f81265b0ea973
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-09-03 20:57:18 +0200

    Created row component for personal page

---
+++ src/pages/personalpage/FristRowContainer.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+import style from "./index.module.scss";
+
+type Prop = {
+
+}
+
+const FristRowContainer: FC<Prop> = () => {
+    return (
+        <div className={style.fristRowContainer}>
+            <div></div>
+        </div>
+    )
+}
+
+export default FristRowContainer;
