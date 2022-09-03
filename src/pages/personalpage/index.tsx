276e61ab4c13ac32b1dfb1d7c11ea95a65d6e67b
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-09-03 20:57:59 +0200

    Added row in the page

--- src/pages/personalpage/index.tsx
+++ src/pages/personalpage/index.tsx
@@ -2,6 +2,7 @@ import { GetServerSidePropsContext, NextPage } from "next";
 import { getUser } from "../../utils/helpers/getUser";
 import { User } from "../../utils/types";
 import AvatarContainer from "./AvatarContainer";
+import FristRowContainer from "./FristRowContainer";
 import style from "./index.module.scss";
 
 type Prop = {
@@ -13,6 +14,7 @@ const PersonalPage: NextPage<Prop> = ({ user }) => {
     return (
         <div className={style.container}>
             <AvatarContainer user={user} />
+            <FristRowContainer />
         </div >
 
     )
