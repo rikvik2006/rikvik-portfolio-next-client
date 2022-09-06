0617caece4d443e5882a17e40e56ec8b24890463
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-09-06 22:37:40 +0200

    Trying to pass the user props got by a getServerSideProps function by component but this doesn't work, i'il try leater

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,12 +2,15 @@ import { ReactElement } from "react";
 import style from "./layout.module.scss";
 import Navbar from "../misc/Navbar";
 import FooterComponet from "../misc/Footer";
+import { getUserOrFalse } from "../../utils/helpers/getUserOrFalse";
+import { User } from "../../utils/types";
+import { GetServerSidePropsContext } from "next";
 
-function Layout({ children }: { children: ReactElement }) {
+function Layout({ children, user }: { children: ReactElement, user: User }) {
     return (
         <>
             <div className={style.layoutStyle}>
-                <Navbar />
+                <Navbar user={user} />
                 <main>{children}</main>
                 <FooterComponet />
             </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,34 @@
 import "../utils/styles/globals.scss"
 import type { AppProps } from 'next/app'
 import { Layout } from "../components"
-import { NextPage } from 'next';
+import { GetServerSidePropsContext, NextPage } from 'next';
 import { ReactElement, ReactNode } from 'react';
+import { getUserOrFalse } from "../utils/helpers/getUserOrFalse";
+import { User } from "../utils/types";
 
 type NextPageWithLayout = NextPage & {
     getLayout?: (page: ReactElement) => ReactNode
 }
 
 type ApppropsWithLayout = AppProps & {
-    Component: NextPageWithLayout
+    Component: NextPageWithLayout;
+    user: User;
 }
 
 function MyApp({ Component, pageProps }: ApppropsWithLayout) {
-    const getLayout = Component.getLayout ?? ((page) => <Layout>{page}</Layout>);
+    const getLayout = Component.getLayout ?? ((page) => <Layout user={pageProps}>{page}</Layout>);
+    console.log("---User From app---")
+    console.log(pageProps);
 
     return (
         getLayout(<Component {...pageProps} />)
     )
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+    console.log("---Context---")
+    console.log(context);
+    return getUserOrFalse(context);
 }
 
 export default MyApp
