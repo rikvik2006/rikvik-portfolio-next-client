5060ce7aa1f5d2f4d82cdcbc1080bfa7d6db79ae
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-09-06 22:39:16 +0200

    Created the component for show if you are logged the icon for go in your personal page, or if you aren't logged the icon icon for login or register

---
+++ src/components/misc/Navbar/Contenitore link/LoginTools/loginOrPersonalPage.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { FC, ReactElement } from "react";
+import { TbLogin, TbUserCircle } from "react-icons/tb";
+import { User } from "../../../../../utils/types";
+import style from "./index.module.scss";
+
+type UserProp = {
+    user: User;
+}
+
+
+const LoginOrPersonalPage: FC<UserProp> = ({ user }) => {
+
+    const personalPageLink = "/personalpage"
+    const loginLink = "/auth/login"
+    console.log("---User---")
+    console.log(user);
+
+    return (
+        <Link href={user ? personalPageLink : loginLink} className={style.icons}>
+            <a>
+                {user ? <TbUserCircle className={style.icon} /> : <TbLogin className={style.icon} />}
+            </a>
+        </Link>
+    )
+}
+
+export default LoginOrPersonalPage
