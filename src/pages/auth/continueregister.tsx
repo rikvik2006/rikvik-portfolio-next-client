05a9fc9072576583afe954e4ecf924bbf438a636
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-09-06 22:40:34 +0200

    Renamed a file that i called it bad

--- src/pages/auth/continueregister.tsx
+++ src/pages/auth/continueregister.tsx
@@ -3,11 +3,11 @@ import style from "./index.module.scss";
 import ErrorAllert from "../../components/errrorAllert";
 import EmailField from '../../components/login/emailFiled';
 import PasswordFiled from '../../components/login/passwordFiled';
-import { handleSubmitLoginContinue } from '../../utils/helpers/handleSubmitLogin';
 import Link from 'next/link';
 import { TbUser } from 'react-icons/tb';
 import NameSurname from '../../components/login/nameSurname';
 import UsernameField from '../../components/login/usernameField';
+import { handleSubmitRegisterContinue } from '../../utils/helpers/hadleSubmitResisterPage';
 
 type Props = {}
 
@@ -17,7 +17,7 @@ const ContinueLogin: NextPage<Props> = () => {
         <>
             <ErrorAllert error="An error occured, retry leater" />
             <div className={style.container}>
-                <form onSubmit={handleSubmitLoginContinue} className={style.form} >
+                <form onSubmit={handleSubmitRegisterContinue} className={style.form} >
                     <TbUser className={style.icon} />
                     <h3>Set your information</h3>
                     <NameSurname />
