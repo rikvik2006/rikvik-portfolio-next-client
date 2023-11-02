fde4aad392e6fd00c73b53666fb8fcf0bad8f98b
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2023-11-02 23:21:07 +0100

    feat: Creating the project page

---
+++ src/components/projectPage/index.tsx
@@ -0,0 +1,11 @@
+import style from "./index.module.scss";
+
+const ProjectCard = () => {
+    return (
+        <div className={style.container}>
+            <div className={style.imageContainer}>
+
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import style from "./index.module.scss";
+
+const ProjectPage: NextPage = () => {
+    return (
+        <div className={style.container}>
+            <div>Ciao</div>
+            {/* <div>Ciao</div> */}
+        </div>
+    )
+}
+
+export default ProjectPage
