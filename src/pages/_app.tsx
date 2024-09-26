99759d7134a8d712e33b7e6725769263bd357276
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2024-09-26 02:39:05 +0200

    chore: linked Cubano font with https link

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,26 @@
-import "../utils/styles/globals.scss"
-import type { AppProps } from 'next/app'
-import { Layout } from "../components"
-import { GetServerSidePropsContext, NextPage } from 'next';
-import { ReactElement, ReactNode, useEffect, useState } from 'react';
+import "../utils/styles/globals.scss";
+import type { AppProps } from "next/app";
+import { Layout } from "../components";
+import { GetServerSidePropsContext, NextPage } from "next";
+import { ReactElement, ReactNode, useEffect, useState } from "react";
 import { getUserOrFalse } from "../utils/helpers/getUserOrFalse";
 import { User } from "../utils/types";
-import toast, { Toaster, useToasterStore } from "react-hot-toast"
+import toast, { Toaster, useToasterStore } from "react-hot-toast";
 import { ConditionalRenderingContext } from "../utils/contexts/conditionalRendering";
 
 type NextPageWithLayout = NextPage & {
-    getLayout?: (page: ReactElement) => ReactNode
-}
+    getLayout?: (page: ReactElement) => ReactNode;
+};
 
 type ApppropsWithLayout = AppProps & {
     Component: NextPageWithLayout;
-}
+};
 
 function MyApp({ Component, pageProps }: ApppropsWithLayout) {
-    const [renderServicies, setRenderServicies] = useState<boolean>(true)
+    const [renderServicies, setRenderServicies] = useState<boolean>(true);
 
-    const getLayout = Component.getLayout ?? ((page) => <Layout >{page}</Layout>);
+    const getLayout =
+        Component.getLayout ?? ((page) => <Layout>{page}</Layout>);
 
     const { toasts } = useToasterStore();
 
@@ -27,25 +28,27 @@ function MyApp({ Component, pageProps }: ApppropsWithLayout) {
         toasts
             .filter((t) => t.visible)
             .filter((_, i) => i >= 5)
-            .forEach((t) => toast.dismiss(t.id))
-    }, [toasts])
+            .forEach((t) => toast.dismiss(t.id));
+    }, [toasts]);
 
     return (
         <>
-            <ConditionalRenderingContext.Provider value={{ renderServicies, setRenderServicies }}>
+            <ConditionalRenderingContext.Provider
+                value={{ renderServicies, setRenderServicies }}
+            >
                 {getLayout(<Component {...pageProps} />)}
-                <Toaster toastOptions={
-                    {
+                <Toaster
+                    toastOptions={{
                         style: {
                             backgroundColor: "#2a2e35",
                             color: "#f8f8f8",
-                            border: "3px solid #f8f8f830"
+                            border: "3px solid #f8f8f830",
                         },
-                    }
-                } />
+                    }}
+                />
             </ConditionalRenderingContext.Provider>
         </>
-    )
+    );
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
     render() {
@@ -7,25 +6,40 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link
-                        href="http://fonts.cdnfonts.com/css/cubano"
+                        href="https://fonts.cdnfonts.com/css/cubano"
                         rel="stylesheet"
                     />
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.googleapis.com"
+                    />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+                        rel="stylesheet"
+                    />
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.googleapis.com"
+                    />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;1,500&display=swap" rel="stylesheet" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;1,500&display=swap"
+                        rel="stylesheet"
+                    />
 
-                    <link href="https://fonts.googleapis.com/css2?family=DynaPuff&display=swap" rel="stylesheet" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=DynaPuff&display=swap"
+                        rel="stylesheet"
+                    />
                 </Head>
                 <body>
                     <Main />
                     <NextScript />
                 </body>
             </Html>
-        )
+        );
     }
 }
 
-export default MyDocument
+export default MyDocument;
